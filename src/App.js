da3e294876afa073e131b2a2404d00dc5b423efe
David <davidq@gmail.com>
2020-06-04 19:48:30 -0400

    troubleshoot Home.js - 3

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         </div>
       <header className="App-header">
         <Switch>
-          <Route path="/cert" component={HomePage} />
+          <Route path="/cert" component={HomePage} exact />
           <Route path="/cert/quote" component={QuoteGen} />
           <Route path="/cert/sol" component={Sol} /> 
           <Route path="/cert/markdown" component={MarkdownPreviwer} /> 
