2856fa7aaa6e18f12b68464afb6d71ff145861b5
David <davidq@gmail.com>
2020-06-08 16:50:18 -0400

    troubleshoot relative path on HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import SolCount from '../components/solCount';
-import QuoteBox from '../components/QuoteBox';
+import SolCount from '../components/solCount.jsx';
+import QuoteBox from '../components/QuoteBox.jsx';
 
 const HomePage = () => (
     <div id="home">
