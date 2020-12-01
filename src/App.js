ced68c72508ff076593d1b5362b64f305595d5c1
David Quintero <davidq@gmail.com>
2020-12-01 00:09:28 -0500

    change routes to match /projects/fcc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-import NavBar from "./components/NavBar";
-import HomePage from "./pages/HomePage";
-import QuoteGen from "./pages/Quotes";
-import Sol from "./pages/Sol";
-import MarkdownPreviwer from "./pages/Markdown";
-import DrumMachine from "./components/DrumMachine";
-import Calculator from "./components/Calculator";
-import Pomodoro from "./components/Pomodoro";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import QuoteGen from './pages/Quotes';
+import Sol from './pages/Sol';
+import MarkdownPreviwer from './pages/Markdown';
+import DrumMachine from './components/DrumMachine';
+import Calculator from './components/Calculator';
+import Pomodoro from './components/Pomodoro';
 
 function App() {
   return (
@@ -20,12 +20,12 @@ function App() {
         <header className="App-header">
           <Switch>
             <Route path="/" component={HomePage} exact />
-            <Route path="/cert/quote" component={QuoteGen} />
-            <Route path="/cert/sol" component={Sol} />
-            <Route path="/cert/markdown" component={MarkdownPreviwer} />
-            <Route path="/cert/drum" component={DrumMachine} />
-            <Route path="/cert/calc" component={Calculator} />
-            <Route path="/cert/pomodoro" component={Pomodoro} />
+            <Route path="/projects/fcc/quote" component={QuoteGen} />
+            <Route path="/projects/fcc/sol" component={Sol} />
+            <Route path="/projects/fcc/markdown" component={MarkdownPreviwer} />
+            <Route path="/projects/fcc/drum" component={DrumMachine} />
+            <Route path="/projects/fcc/calc" component={Calculator} />
+            <Route path="/projects/fcc/pomodoro" component={Pomodoro} />
           </Switch>
         </header>
       </Router>
