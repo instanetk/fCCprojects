179059ced463572378cd80a6650f5289d7f7627f
David Quintero <davidq@gmail.com>
2020-12-01 00:15:11 -0500

    update routes in NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/projects/fcc">Home</Link>
         </li>
         <li>
-          <Link to="/cert/quote">Quote Generator</Link>
+          <Link to="/projects/fcc/quote">Quote Generator</Link>
         </li>
         <li>
-          <Link to="/cert/markdown">Markdown</Link>
+          <Link to="/projects/fcc/markdown">Markdown</Link>
         </li>
         <li>
-          <Link to="/cert/drum">Drum Machine</Link>
+          <Link to="/projects/fcc/drum">Drum Machine</Link>
         </li>
         <li>
-          <Link to="/cert/calc">Calculator</Link>
+          <Link to="/projects/fcc/calc">Calculator</Link>
         </li>
         <li>
-          <Link to="/cert/pomodoro">Pomodoro</Link>
+          <Link to="/projects/fcc/pomodoro">Pomodoro</Link>
         </li>
         <li>
-          <Link to="/cert/sol">SOL</Link>
+          <Link to="/projects/fcc/sol">SOL</Link>
         </li>
       </ul>
     </nav>
